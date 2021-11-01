79f29f04c9a0787a158402ac2b3ac0d7410e8752
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2021-11-01 10:39:53 -0400

    Update input.c

--- Project2/input.c
+++ Project2/input.c
@@ -2,19 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define PIE 3.14
 
-//Tests declaring integer global variable functionality
-short sa;
-
-//Tests assigning to integer global variable functionality
-sa = 1;
-
 //Tests declaring and assigning to integer global variable functionality
+short sa = 1;
 short sb = 2;
 
-
-int main(int argc, char *argv[]) 
+//Tests main functionality
+int main() 
 {
 	//Tests declaring integer inside main functionality
 	short sc;
@@ -26,17 +22,34 @@ int main(int argc, char *argv[])
 	short sd = 4;
 	
 	
-	//Tests addition functionality
-	int addnum = 5 + 7;
+	//Tests scanf inputting integers functionality
+	short inptnum;
+	printf("Input an integer: ");
+	scanf("%hd", &inptnum);
 	
-	//Tests printf outputting integersfunctionality
-	printf("Sum: %d", addnum);
+	//Tests addition functionality
+	short addnum = 5 + 7;
+	
+	//Tests printf outputting integers functionality
+	printf("Sum: %hd\n", addnum);
 	
 	//Tests subtraction functionality
-	int subnum = 9 - 7;
+	short subnum = 9 - 7;
 	
 	//Tests printf outputting integers functionality again
-	printf("Difference: %d", subnum);
+	printf("Difference: %hd\n", subnum);
+	
+	//Test AND functionality
+	bool andbool = (0 && 1);
+	printf("andbool: %hd\n", andbool);
+	
+	//Test OR functionality
+	bool orbool = (0 || 1);
+	printf("orbool: %hd\n", orbool);
+	
+	//Test NEG functionality
+	bool negbool = !(0 || 1);
+	printf("negbool: %hd\n", negbool);
 	
 	
 	//Tests return statement functionality
